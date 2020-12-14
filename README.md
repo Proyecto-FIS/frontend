d4aa4263b2ad2bb886a7c1dd304668f4e89db821
AMG <andresmargar98@gmail.com>
2020-12-14 16:00:42 +0100

    [fix] Disabled development proxy in production

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ Create the following files in the project root folder:
 
 A summary of possible environment variables is here:
 - API_ENDPOINT_URL: URL pointing to the Coffaine API service
+
+## Proxy
+To use a development proxy, you must create a setupProxy.js file under src folder,
+Then, enter this content:
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: process.env.API_ENDPOINT_URL,
+            changeOrigin: true,
+        })
+    );
+};
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,11 @@
-if (process.env.NODE_ENV === "development") {
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-    const { createProxyMiddleware } = require('http-proxy-middleware');
-
-    module.exports = function (app) {
-        app.use(
-            '/api',
-            createProxyMiddleware({
-                target: process.env.API_ENDPOINT_URL,
-                changeOrigin: true,
-            })
-        );
-    };
-}
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: process.env.API_ENDPOINT_URL,
+            changeOrigin: true,
+        })
+    );
+};
