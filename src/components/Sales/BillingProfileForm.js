c55a24406d5420be204c18a3f33ab3133630aadb
AMG <andresmargar98@gmail.com>
2020-12-28 14:09:45 +0100

    [feat] Finished BillingProfile form

--- src/components/Sales/BillingProfileForm.js
+++ src/components/Sales/BillingProfileForm.js
@@ -1,6 +1,55 @@
 import React, { Component } from "react";
-import { TextField, Button, Grid } from "@material-ui/core";
+import { TextField, Button, Grid, Box } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
+import Validators from "../../utils/Validators";
+
+const fields = [
+    {
+        label: "Nombre",
+        name: "name",
+        validators: [Validators.StringLength(1, 100)]
+    },
+    {
+        label: "Apellidos",
+        name: "surname",
+        validators: [Validators.StringLength(1, 100)]
+    },
+    {
+        label: "Dirección",
+        name: "address",
+        validators: [Validators.StringLength(1, 150)]
+    },
+    {
+        label: "Ciudad",
+        name: "city",
+        validators: [Validators.StringLength(1, 100)]
+    },
+    {
+        label: "Provincia",
+        name: "province",
+        validators: [Validators.StringLength(1, 100)]
+    },
+    {
+        label: "País",
+        name: "country",
+        validators: [Validators.StringLength(1, 100)]
+    },
+    {
+        label: "Código postal",
+        name: "zipCode",
+        validators: [Validators.CheckInteger(), Validators.TestRegex(/^([0-9]{5}$)/)]
+    },
+    {
+        label: "Número de teléfono",
+        name: "phoneNumber",
+        validators: [Validators.TestRegex(/^([0-9]{1,15}$)/)]
+    },
+    {
+        label: "Correo electrónico",
+        name: "email",
+        validators: [Validators.TestRegex(/^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/)]
+    },
+];
 
 const styles = (theme) => ({
     titleText: {
@@ -9,63 +58,16 @@ const styles = (theme) => ({
     form: {
         display: "flex",
         flexDirection: "column",
-        spacing: 2
     }
 });
 
-const FormTextField = (props) => {
-    return <TextField label={props.label} error={props.errorFlag} variant="outlined" required value={props.value} onChange={props.onChange} />;
-};
+const FormTextField = (props) => (
+    <TextField label={props.label} error={props.error !== ""}
+        variant="outlined" value={props.value}
+        onChange={props.onChange} helperText={props.error} fullWidth={true} />
+);
 
 class BillingProfileForm extends Component {
-
-    fields = [
-        {
-            label: "Nombre",
-            name: "name",
-            validate: (v) => { return true },
-        },
-        {
-            label: "Apellidos",
-            name: "surname",
-            validate: (v) => { return true },
-        },
-        {
-            label: "Dirección",
-            name: "address",
-            validate: (v) => { return true },
-        },
-        {
-            label: "Ciudad",
-            name: "city",
-            validate: (v) => { return true },
-        },
-        {
-            label: "Provincia",
-            name: "province",
-            validate: (v) => { return true },
-        },
-        {
-            label: "País",
-            name: "country",
-            validate: (v) => { return true },
-        },
-        {
-            label: "Código postal",
-            name: "zipCode",
-            validate: (v) => { return true },
-        },
-        {
-            label: "Número de teléfono",
-            name: "phoneNumber",
-            validate: (v) => { return true },
-        },
-        {
-            label: "Correo electrónico",
-            name: "email",
-            validate: (v) => { return true },
-        },
-    ];
 
     constructor(props) {
         super(props);
@@ -78,7 +80,7 @@ class BillingProfileForm extends Component {
 
     getDefaultState() {
         let state = { formCorrect: false };
-        this.fields.forEach(field => state[field.name] = { value: "", errorFlag: false });
+        fields.forEach(field => state[field.name] = { value: "", error: "" });
         return state;
     }
 
@@ -88,30 +90,51 @@ class BillingProfileForm extends Component {
 
     setField(field, e) {
         let newState = {};
-        newState[field.name] = {};
-        newState[field.name].value = e.target.value;
-        newState[field.name].errorFlag = !field.validate(e.target.value);
-        newState.formCorrect = this.fields.reduce((ac, v) => this.state[v.name].errorFlag ? false : ac, true);
+        newState[field.name] = {
+            value: e.target.value,
+            error: Validators.validate(field.validators, e.target.value)
+        };
         this.setState(newState);
+        this.setState((prevState) => ({
+            formCorrect: fields.reduce((ac, v) => (prevState[v.name].error !== "" || prevState[v.name].value === "") ? false : ac, true)
+        }));
     }
 
     render() {
         const { classes } = this.props;
 
-        const formFields = this.fields.map(field => <FormTextField label={field.label} errorFlag={this.state[field.name].errorFlag} value={this.state[field.name].value} onChange={this.setField.bind(this, field)} />);
+        const formFields = fields.map((field, index) => (
+            <Grid key={index} item xs={12}>
+                <FormTextField label={field.label} error={this.state[field.name].error}
+                    value={this.state[field.name].value}
+                    onChange={this.setField.bind(this, field)} />
+            </Grid>
+        ));
 
         return (
             <Grid container>
-                <Grid container item sm={2} xs={1}></Grid>
+                <Grid item sm={2} xs={1}></Grid>
                 <Grid item sm={8} xs={10}>
-                    <h2 className={classes.titleText}>Añadir nuevo perfil de entrega</h2>
-                    <form className={classes.form}>
-                        {formFields}
-                        <Button variant="contained" color="secondary" disabled={!this.state.formCorrect} onClick={e => this.submitForm()}>Añadir</Button>
-                        <Button variant="contained" color="secondary" onClick={e => this.clearForm()}>Limpiar</Button>
-                    </form>
+                    <Box border={1} borderRadius={10} boxShadow={3} padding={3}>
+                        <h2 className={classes.titleText}>Añadir nuevo perfil de entrega</h2>
+                        <Grid container>
+                            <Grid item sm={2} xs={2}></Grid>
+                            <Grid container item xs={8} sm={8} spacing={3} className={classes.form}>
+                                {formFields}
+                                <Grid container item spacing={4} direction="row-reverse">
+                                    <Grid item>
+                                        <Button variant="contained" color="secondary" onClick={e => this.clearForm()}>Limpiar</Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" color="secondary" disabled={!this.state.formCorrect} onClick={e => this.submitForm()}>Añadir</Button>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid item sm={2} xs={2}></Grid>
+                        </Grid>
+                    </Box>
                 </Grid>
-                <Grid container item sm={2} xs={1}></Grid>
+                <Grid item sm={2} xs={1}></Grid>
             </Grid>
         );
     }
