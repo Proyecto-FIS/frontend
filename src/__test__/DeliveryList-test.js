d18517876c5d4d275038a5d5cf24ab448f2c005f
danmarrod <danmarrod@alum.us.es>
2020-12-29 13:48:57 +0100

    Karma and Mocha basic config

---
+++ src/__test__/DeliveryList-test.js
@@ -0,0 +1,14 @@
+var React = require('react/addons'),
+    ScatterPlot = require('../components/Deliveries/DeliveriesList'),
+    TestUtils = React.addons.TestUtils;
+
+
+describe('DeliveriesList', function () {
+
+    });
+
+    it("renders an h1", function () {
+
+    });
+
+});
