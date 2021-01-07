6bf9183bbe1a064c84386da678ec70d066cd37d6
AMG <andresmargar98@gmail.com>
2021-01-07 22:07:40 +0100

    [fix] Fixed server for React Router

--- src/productionServer.js
+++ src/productionServer.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const path = require("path");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
+
+const STATIC = path.resolve(__dirname, "..", "build");
+const INDEX = path.resolve(STATIC, "index.html");
 
 app.use(
     '/api',
@@ -13,7 +17,11 @@ app.use(
     })
 );
 
-app.use("/", express.static(__dirname + "/../build"));
+app.use(express.static(STATIC));
+
+app.get("*", (req, res) => {
+    res.sendFile(INDEX);
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Running at port ${process.env.PORT}`);
