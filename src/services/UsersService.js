59b024f1fe0a741dd1a669e70c51ceafa39f0e43
1nt0 <webdevelopjajo@gmail.com>
2021-01-16 15:46:31 +0100

    Refactored UsersService

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -3,7 +3,15 @@ import store from "../redux/store";
 import { LOGIN_SUCCESS, LOGIN_ERROR, LOGIN_REQUEST, LOGOUT, REGISTER_SUCCESS, REGISTER_ERROR, REGISTER_REQUEST, PROFILE_REQUEST, PROFILE_SUCCESS, PROFILE_ERROR, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_ERROR, UPDATE_PROFILE_REQUEST, GET_TOASTERS_SUCCESS, REQUEST_TOASTERS, TOASTER_PRODUCTS_ERROR, TOASTER_PRODUCTS_SUCCESS, TOASTER_PRODUCTS_REQUEST } from "../redux/actions/types";
 import startSnackBar from "../redux/actions/SnackBar/startSnackBar";
 
+const config = {
+    headers: {
+      'Content-Type':'application/json'
+    }
+  };
+
+
 export class UsersService {
+
     static getUserToken = () => {
         const account = store.getState().AuthReducer.account;
         if(account) {
@@ -16,27 +24,26 @@ export class UsersService {
 
     // --------- AUTH -----------
     static login = (body) => {
-        const config = {
-            headers: {
-              'Content-Type':'application/json'
-            }
-          };
-
-        store.dispatch({type: LOGIN_REQUEST});
-        axios.post('/api/auth/login', body, config)
-        
-            .then(response => {
-                store.dispatch({type: LOGIN_SUCCESS, payload: response.data})
-                localStorage.setItem('account', JSON.stringify(response.data))
-        })
-            .catch(err =>{ 
-                const errors = err.response.data.errors;
-                if (errors) {
-                    errors.forEach(error => store.dispatch(startSnackBar("error", error.msg)));
-                  }
-              
-                  store.dispatch({type: LOGIN_ERROR});
+        return new Promise((resolve, reject) => {
+            store.dispatch({type: LOGIN_REQUEST});
+            
+            axios.post('/api/auth/login', body, config)
+            
+                .then(response => {
+                    store.dispatch({type: LOGIN_SUCCESS, payload: response.data})
+                    localStorage.setItem('account', JSON.stringify(response.data))
+                    resolve()
             })
+                .catch(err =>{ 
+                    const errors = err.response.data.errors;
+                    if (errors) {
+                        errors.forEach(error => store.dispatch(startSnackBar("error", error.msg)));
+                    }
+                
+                    store.dispatch({type: LOGIN_ERROR});
+                    reject()
+                })
+        })
     }
 
     static logOut = () => {
@@ -47,187 +54,187 @@ export class UsersService {
 
     // --------- CUSTOMER -----------
     static registerCustomer = (body) => {
-        const config = {
-            headers: {
-              'Content-Type':'application/json'
-            }
-          };
+        return new Promise((resolve, reject) => {
 
-        store.dispatch({type: REGISTER_REQUEST});
-        axios.post('/api/customers', body, config)
-        
-            .then(response => {
-                store.dispatch({type: REGISTER_SUCCESS, payload: response.data})
-                store.dispatch({type: LOGIN_SUCCESS, payload: response.data})
-                localStorage.setItem('account', JSON.stringify(response.data))
-            })
-            .catch(err => { 
-                store.dispatch(startSnackBar("error", err.response.data.message))
-                store.dispatch({type: REGISTER_ERROR})
-            })
-    }
+            store.dispatch({type: REGISTER_REQUEST});
 
-    static getCustomerProfile = (accountId) => {
+            axios.post('/api/customers', body, config)
+                    .then(response => {
+                        store.dispatch({type: REGISTER_SUCCESS, payload: response.data})
+                        store.dispatch({type: LOGIN_SUCCESS, payload: response.data})
+                        localStorage.setItem('account', JSON.stringify(response.data))
 
-        const config = {
-            headers: {
-              'Content-Type':'application/json'
-            }
-          }
-          
+                        resolve()
+                    })
+                    .catch(err => { 
+                        store.dispatch(startSnackBar("error", err.response.data.message))
+                        store.dispatch({type: REGISTER_ERROR})
+
+                        reject()
+                    })
+                })
+    };
+
+    static getCustomerProfile = (accountId) => { 
         store.dispatch({type: PROFILE_REQUEST});
         axios.get(`/api/customers/${accountId}`, config)
         
             .then(response => {
                 store.dispatch({type: PROFILE_SUCCESS, payload: response.data})
+
             })
             .catch(err => { 
                 store.dispatch(startSnackBar("error", err.message))
-              
+            
                 store.dispatch({type: PROFILE_ERROR})
             })
-
-    }
+    };
 
     static updateCustomerProfile = (accountId, body) => {
-        const config = {
-            headers: {
-              'Content-Type':'application/json'
+        return new Promise((resolve, reject) => {
+            
+            const userToken = UsersService.getUserToken();
+            if (!userToken) {
+              reject();
+              return;
             }
-          };
 
-          const {AuthReducer: {account}} = store.getState();
+            //Adding userToken to body
+            var objBody = JSON.parse(body);
+            objBody.userToken = userToken;
+            var newBody = JSON.stringify(objBody);
 
-          const token = account.token;
 
-          axios.get(`/api/auth/${token}`, config)
+            axios.get(`/api/auth/${userToken}`, config)
             .then(response => {
                 const { account_id: loggedAccountId } = response.data;
 
                 if (accountId !== loggedAccountId) {
                     store.dispatch(startSnackBar("error", "Operación no autorizada"))
-                    store.dispatch({type: LOGOUT})
-
+                    reject();
                 } else {
                     store.dispatch({type: UPDATE_PROFILE_REQUEST})
 
-                    axios.put(`/api/customers/${accountId}`, body, config)
+                    axios.put(`/api/customers/${accountId}`, newBody, config)
                     .then(response => {
                         store.dispatch({type: UPDATE_PROFILE_SUCCESS, payload: response.data})
                         store.dispatch(startSnackBar("success", "Perfil actualizado correctamente"))
+
+                        resolve();
                     })
                     .catch(err => { 
                         store.dispatch(startSnackBar("error", err.response.data.message))
                         store.dispatch({type: UPDATE_PROFILE_ERROR})
-                    })
-                    
+
+                        reject();
+                    })  
                 }
             })
             .catch(err => { 
                 store.dispatch(startSnackBar("error", "Token inválido, vuelve a iniciar sesión"))
                 store.dispatch({type: UPDATE_PROFILE_ERROR})
                 store.dispatch({type: LOGOUT})
-                
+
+                reject()
             })
-          
+
+        });     
     }
 
 
     // --------- TOASTER -----------
     static registerToaster = (body) => {
-        const config = {
-            headers: {
-              'Content-Type':'application/json'
-            }
-          };
+        return new Promise((resolve, reject) => {
+            store.dispatch({type: REGISTER_REQUEST});
 
-        store.dispatch({type: REGISTER_REQUEST});
-        axios.post('/api/toasters', body, config)
-        
-            .then(response => {
-                store.dispatch({type: REGISTER_SUCCESS, payload: response.data})
-                store.dispatch({type: LOGIN_SUCCESS, payload: response.data})
-                localStorage.setItem('account', JSON.stringify(response.data))
-            })
-            .catch(err => { 
-                store.dispatch(startSnackBar("error", err.response.data.message))
-              
-                store.dispatch({type: REGISTER_ERROR})
-            })
+            axios.post('/api/toasters', body, config)
+                .then(response => {
+                    store.dispatch({type: REGISTER_SUCCESS, payload: response.data})
+                    store.dispatch({type: LOGIN_SUCCESS, payload: response.data})
+                    localStorage.setItem('account', JSON.stringify(response.data))
+                    resolve()
+                })
+                .catch(err => { 
+                    store.dispatch(startSnackBar("error", err.response.data.message))
+                
+                    store.dispatch({type: REGISTER_ERROR})
+                    reject()
+                })
+            });
     }
 
     static getToasterProfile = (accountId) => {
-
-        const config = {
-            headers: {
-              'Content-Type':'application/json'
-            }
-          }
-          
         store.dispatch({type: PROFILE_REQUEST});
+
         axios.get(`/api/toasters/${accountId}`, config)
-        
             .then(response => {
                 store.dispatch({type: PROFILE_SUCCESS, payload: response.data})
-
                 store.dispatch({type: TOASTER_PRODUCTS_REQUEST});
+
                 axios.get(`/api/products?providerId=${accountId}`, config)
                     .then(response => {
                         store.dispatch({type: TOASTER_PRODUCTS_SUCCESS, payload: response.data})
                     })
                     .catch(err => {
                         store.dispatch(startSnackBar("error", err))
-              
+            
                         store.dispatch({type: TOASTER_PRODUCTS_ERROR})
                     })
             })
             .catch(err => { 
                 store.dispatch(startSnackBar("error", err))
-              
+            
                 store.dispatch({type: PROFILE_ERROR})
             })
-
     }
 
     static updateToasterProfile = (accountId, body) => {
-        const config = {
-            headers: {
-              'Content-Type':'application/json'
+        return new Promise((resolve, reject) => {
+            const userToken = UsersService.getUserToken();
+            if (!userToken) {
+            reject();
+            return;
             }
-          };
 
-          const {AuthReducer: {account}} = store.getState();
+            //Adding userToken to body
+            var objBody = JSON.parse(body);
+            objBody.userToken = userToken;
+            var newBody = JSON.stringify(objBody);
 
-          const token = account.token;
+            axios.get(`/api/auth/${userToken}`, config)
+                .then(response => {
+                    const { account_id: loggedAccountId } = response.data;
 
-          axios.get(`/api/auth/${token}`, config)
-            .then(response => {
-                const { account_id: loggedAccountId } = response.data;
+                    if (accountId !== loggedAccountId) {
+                        store.dispatch(startSnackBar("error", "Operación no autorizada"))
+                        reject()
+                    } else {
+                        store.dispatch({type: UPDATE_PROFILE_REQUEST})
 
-                if (accountId !== loggedAccountId) {
-                    store.dispatch(startSnackBar("error", "Operación no autorizada"))
+                        axios.put(`/api/toasters/${accountId}`, newBody, config)
+                        .then(response => {
+                            store.dispatch({type: UPDATE_PROFILE_SUCCESS, payload: response.data})
+                            store.dispatch(startSnackBar("success", "Perfil actualizado correctamente"))
+
+                            resolve()
+                        })
+                        .catch(err => { 
+                            store.dispatch(startSnackBar("error", err.response.data.message))
+                            store.dispatch({type: UPDATE_PROFILE_ERROR})
+
+                            reject()
+                        })
+                        
+                    }
+                })
+                .catch(err => { 
+                    store.dispatch(startSnackBar("error", "Token inválido, vuelve a iniciar sesión"))
+                    store.dispatch({type: UPDATE_PROFILE_ERROR})
                     store.dispatch({type: LOGOUT})
 
-                } else {
-                    store.dispatch({type: UPDATE_PROFILE_REQUEST})
-
-                    axios.put(`/api/toasters/${accountId}`, body, config)
-                    .then(response => {
-                        store.dispatch({type: UPDATE_PROFILE_SUCCESS, payload: response.data})
-                        store.dispatch(startSnackBar("success", "Perfil actualizado correctamente"))
-                    })
-                    .catch(err => { 
-                        store.dispatch(startSnackBar("error", err.response.data.message))
-                        store.dispatch({type: UPDATE_PROFILE_ERROR})
-                    })
+                    reject()
                     
-                }
-            })
-            .catch(err => { 
-                store.dispatch(startSnackBar("error", "Token inválido, vuelve a iniciar sesión"))
-                store.dispatch({type: UPDATE_PROFILE_ERROR})
-                store.dispatch({type: LOGOUT})
-                
+                })
             })
           
     }
